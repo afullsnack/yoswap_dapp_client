c8f47c560e22e77a66dfc3a4514d8267f029f634
Miracle Friday <miraclef60@gmail.com>
2025-09-01 13:46:08 +0100

    refactor: Update tags list

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -32,7 +32,7 @@ export async function GET() {
       homeUrl: URL,
       webhookUrl: `${URL}/api/webhook`,
       primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-      tags: ["YoSwap", "cNGN", "Miniapp", "Dollar", "Naira", "Stablecoin"],
+      // tags: ["YoSwap", "cNGN", "Miniapp", "Dollar", "Naira", "Stablecoin"],
       heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
       tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
       ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
