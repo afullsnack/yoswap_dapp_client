f802f6d7809c739946656e71fc59ea62cab30ace
Miracle Friday <miraclef60@gmail.com>
2025-09-01 14:14:28 +0100

    fix: Fix invalid prop in HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" size="any" />
+        <link rel="icon" href="/favicon.png" sizes="any" />
       </head>
       <body>
         <MiniKitContextProvider>
